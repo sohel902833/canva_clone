2ecab14c37115494640723ed72ee097469c9853d
sohel902833 <mdsohrabhossainsohel902802@gmail.com>
2025-03-02 06:44:14 +0600

    implemented a circile moving inside canvas nicely

--- html-canvas/src/main.js
+++ html-canvas/src/main.js
@@ -1,6 +1,8 @@
 import "./assets/index.css";
 
 function main() {
+    const canvasHeight = 600;
+    const canvasWidth = 600;
     const canvas = document.getElementById("canvas");
 
     // canvas.width = window.innerWidth;
@@ -9,42 +11,88 @@ function main() {
     const context = canvas.getContext("2d");
 
     //rectangle
-    for (let i = 50; i <= 400; i += 110) {
-        //x , y, height, width
-        context.fillStyle = "green";
-        context.fillRect(i, i, 100, 100);
-    }
+    // for (let i = 50; i <= 400; i += 110) {
+    //     //x , y, height, width
+    //     context.fillStyle = "green";
+    //     context.fillRect(i, i, 100, 100);
+    // }
 
     //line
-    context.beginPath();
-    //x and y coordinates
-    context.moveTo(200, 100);
-    context.lineTo(500, 100);
-    context.lineTo(500, 400);
-    context.lineTo(200, 100);
-    context.strokeStyle = "blue";
-    context.stroke();
+    // context.beginPath();
+    // //x and y coordinates
+    // context.moveTo(200, 100);
+    // context.lineTo(500, 100);
+    // context.lineTo(500, 400);
+    // context.lineTo(200, 100);
+    // context.strokeStyle = "blue";
+    // context.stroke();
 
     //arc /circle
-    context.beginPath();
-    context.strokeStyle = "black";
-    context.arc(200, 300, 70, 0, Math.PI * 0.6, false);
-    context.stroke();
-    //second one
-    context.beginPath();
-    context.strokeStyle = "red";
-    context.arc(200, 300, 70, Math.PI * 0.6, Math.PI * 0.9, false);
-    context.stroke();
-    //second one
-    context.beginPath();
-    context.strokeStyle = "blue";
-    context.arc(200, 300, 70, Math.PI * 0.9, Math.PI * 1.2, false);
-    context.stroke();
+    // context.beginPath();
+    // context.strokeStyle = "black";
+    // context.arc(200, 300, 70, 0, Math.PI * 0.6, false);
+    // context.stroke();
+    // //second one
+    // context.beginPath();
+    // context.strokeStyle = "red";
+    // context.arc(200, 300, 70, Math.PI * 0.6, Math.PI * 0.9, false);
+    // context.stroke();
+    // //second one
+    // context.beginPath();
+    // context.strokeStyle = "blue";
+    // context.arc(200, 300, 70, Math.PI * 0.9, Math.PI * 1.2, false);
+    // context.stroke();
+    // //third one
+    // context.beginPath();
+    // context.strokeStyle = "blue";
+    // context.arc(200, 300, 70, Math.PI * 1.2, Math.PI * 2, false);
+    // context.stroke();
+
+    //animating circle
     //third one
-    context.beginPath();
-    context.strokeStyle = "blue";
-    context.arc(200, 300, 70, Math.PI * 1.2, Math.PI * 2, false);
-    context.stroke();
+    let x = Math.random() * canvasWidth;
+    let y = Math.random() * canvasHeight;
+
+    let dx = (Math.random() - 0.5) * 10;
+    let dy = (Math.random() - 0.5) * 10;
+    const radius = 30;
+    let isRunning = true;
+    function animate() {
+        if (isRunning) {
+            requestAnimationFrame(animate);
+        }
+        context.clearRect(0, 0, innerWidth, innerHeight);
+        context.beginPath();
+        context.strokeStyle = "blue";
+        context.arc(x, y, radius, 0, Math.PI * 2, false);
+        context.stroke();
+        if (x + radius > canvasWidth || x - radius < 0) {
+            dx = -dx;
+        }
+        if (y + radius > canvasHeight || y - radius < 0) {
+            dy = -dy;
+        }
+        x += dx;
+        y += dy;
+    }
+    animate();
+
+    const startButton = document.getElementById("start_button");
+    const stopButton = document.getElementById("stop_button");
+
+    startButton.addEventListener("click", () => {
+        if (isRunning) {
+            return;
+        }
+        isRunning = true;
+        animate();
+    });
+    stopButton.addEventListener("click", () => {
+        if (!isRunning) {
+            return;
+        }
+        isRunning = false;
+    });
 }
 
 main();
